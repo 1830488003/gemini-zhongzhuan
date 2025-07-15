16920ea0fe48c47e86a5645f008fae33f75c8fb5
1830488003 <1830488003@qq.com>
2025-07-15 14:44:33 +0800

    1

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import worker from "./worker.js";
 
 export default {
   async fetch (req, env, context) {
-    return worker.fetch(req, env, context);
+    const url = new URL(req.url);
+    if (url.pathname.startsWith("/v1/")) {
+      return worker.fetch(req, env, context);
+    }
+    if (env.ASSETS) {
+      // @ts-ignore
+      return env.ASSETS.fetch(req);
+    }
+    return new Response("Not Found", { status: 404 });
   }
 };
